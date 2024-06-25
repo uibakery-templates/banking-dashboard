bb6ccdc4145866043d99227ad9d399386b5b525d
UI Bakery <vika@uibakeryuibakerytest.io>
2024-06-25 12:07:51 +0000

    initial commit

---
+++ pages/home/actions/getMarketData/getData.js
@@ -0,0 +1,27 @@
+return [
+  {
+    "month": "January",
+    "fairValue": 10000,
+    "marketValue": 12000
+  },
+  {
+    "month": "February",
+    "fairValue": 15000,
+    "marketValue": 14000
+  },
+  {
+    "month": "March",
+    "fairValue": 17000,
+    "marketValue": 16000
+  },
+  {
+    "month": "April",
+    "fairValue": 18000,
+    "marketValue": 15000
+  },
+  {
+    "month": "May",
+    "fairValue": 20000,
+    "marketValue": 21000
+  }
+]
---
+++ pages/home/actions/getYearlySummary/getData.js
@@ -0,0 +1,37 @@
+return [
+  {
+    "year": "2018",
+    "revenue": 120000,
+    "expenses": 60000,
+    "netProfit": 60000,
+    "cashFlow": 24000
+  },
+  {
+    "year": "2019",
+    "revenue": 150000,
+    "expenses": 70000,
+    "netProfit": 80000,
+    "cashFlow": 30000
+  },
+  {
+    "year": "2020",
+    "revenue": 170000,
+    "expenses": 80000,
+    "netProfit": 90000,
+    "cashFlow": 35000
+  },
+  {
+    "year": "2021",
+    "revenue": 180000,
+    "expenses": 90000,
+    "netProfit": 90000,
+    "cashFlow": 35000
+  },
+  {
+    "year": "2022",
+    "revenue": 200000,
+    "expenses": 100000,
+    "netProfit": 100000,
+    "cashFlow": 40000
+  }
+];
